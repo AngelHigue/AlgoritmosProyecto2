0f7f65f40aceca40422c099e2a63ed9cd57d8f7f
Ángel Higueros <45275116+AngelHigue@users.noreply.github.com>
2021-06-03 19:22:55 -0600

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  port: process.env.PORT || 8000,
-  host: process.env.HOST || 'http://localhost',
-  publicRoute: process.env.PUBLIC_ROUTE || '/app',
-  filesRouter: process.env.FILES_ROUTER || 'files',
-}
-
-module.exports = config
