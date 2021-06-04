11eb50cd3cbab35676ef89cd34129bac71fe5c42
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2021-06-03 19:40:51 -0600

    Delete Frontend directory

--- Frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Dashboard } from "./Containers/Dashboard";
-import "bulma/css/bulma.css";
-
-ReactDOM.render(<Dashboard />, document.getElementById("root"));
-     
